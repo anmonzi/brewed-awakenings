faf8d282ad1862992da2f6995008a7053142769d
Ashton Monzi <anmonzi@gmail.com>
2021-04-22 14:22:06 -0500

    added event listeners to employees

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,16 +1,43 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js";
 
-const employees = getEmployees()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [, employeeId] = itemClicked.id.split("--")
+
+            for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+                    const orders = getOrders() //need to declare orders with the getOrders function for filter to work
+                    const employeeOrders = orders.filter(  // <--- Go to YouTube and search "javascript array filter"
+                        (order) => {
+                            if (order.employeeId === employee.id) {
+                                return true
+                            }
+                        }
+                    )
+
+                    window.alert(` ${employee.name} sold ${employeeOrders.length} products `)
+                }
+            }
+        }
+    }
+)
+
+
+
+const employees = getEmployees();
 
 export const Employees = () => {
-    let html = "<ul>"
+  let html = "<ul>";
 
-    for (const employee of employees) {
-        html += `<li id="employee--${employee.id}">${employee.name}</li>`
-    }
+  for (const employee of employees) {
+    html += `<li id="employee--${employee.id}">${employee.name}</li>`;
+  }
 
-    html += "</ul>"
+  html += "</ul>";
 
-    return html
-}
-
+  return html;
+};
